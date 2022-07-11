9616a5f79a9a3f59791c0bd2910c977408b2462b
Aravind R <ndarvi48332@gmail.com>
2022-07-11 12:13:32 +0530

    ajax added

--- mysite/login_system/views.py
+++ mysite/login_system/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
 # Create your views here.
+def index(request):
+    if 'username' in request.session:
+        #change reg to home
+        return redirect(reg)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['pass']
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            #create session
+            #change reg to home
+            return redirect(reg)
+    else:
+        return render(request, 'index.html')
+def reg(request):
+    return HttpResponse("<h2>Reg</h2>")
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login_system.apps.LoginSystemConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'login_system_db',
+        'USER': 'postgres',
+        'PASSWORD':'arvi9895',
+        'HOST':'localhost'
     }
 }
 
